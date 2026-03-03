e7a78ebf3f545151798c5ddd388e7fdda9309dd9
dakychan <kotaychinkot4@gmail.com>
2026-03-03 22:19:00 +0500

    D

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@
             clientLinkUrl: "http://localhost:8127/api",
             
             // GitHub Pages base path (leave empty for root domain, or set to "/repo-name" for GitHub Pages)
-            basePath: "",
+            basePath: "/aporia-cc.github.io",
 
             discordLink: "https://t.me/batareikasource",
             telegramLink: "https://t.me/batareikasource",
